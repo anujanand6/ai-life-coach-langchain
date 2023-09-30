67683d4092a4036631d71bf2fc180e4f53b34bc7
Anuj Anand <anujanand6@gmail.com>
2023-09-30 16:10:05 +0530

    Modify Homepage content and add some docs

--- Home.py
+++ Home.py
@@ -1,25 +1,22 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Langchain Chatbot",
+    page_title="AI Life Coach",
     page_icon='💬',
     layout='wide'
 )
 
-st.header("Chatbot Implementations with Langchain")
+st.header("AI Life Coach built using Langchain and Streamlit")
 st.write("""
-[![view source code ](https://img.shields.io/badge/GitHub%20Repository-gray?logo=github)](https://github.com/shashankdeshpande/langchain-chatbot)
-![Visitors](https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Flangchain-chatbot.streamlit.app&label=Visitors&labelColor=%235d5d5d&countColor=%231e7ebf&style=flat)
+[![view source code ](https://img.shields.io/badge/GitHub%20Repository-gray?logo=github)](https://github.com/anujanand6/ai-life-coach-langchain)
 """)
+
 st.write("""
-Langchain is a powerful framework designed to streamline the development of applications using Language Models (LLMs). It provides a comprehensive integration of various components, simplifying the process of assembling them to create robust applications.
+         Welcome to AI Life Coach! 
 
-Leveraging the power of Langchain, the creation of chatbots becomes effortless. Here are a few examples of chatbot implementations catering to different use cases:
-
-- **Basic Chatbot**: Engage in interactive conversations with the LLM.
-- **Context aware chatbot**: A chatbot that remembers previous conversations and provides responses accordingly.
-- **Chatbot with Internet Access**: An internet-enabled chatbot capable of answering user queries about recent events.
-- **Chat with your documents** Empower the chatbot with the ability to access custom documents, enabling it to provide answers to user queries based on the referenced information.
-
-To explore sample usage of each chatbot, please navigate to the corresponding chatbot section.
+         Here are a few coaches catering to different use cases:
+         
+         - **Fitness Health Coach**: A coach that plans your workouts, builds diet plans and helps you achieve your overall physical health goals.
+         
+         To explore sample usage of each coach, please navigate to the corresponding coach on the side.
 """)
---
+++ pages/5_📄_Docs.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="Docs", page_icon="📄")
+st.header('Some helpful documentation on the tools used to build this app.')
+
+st.write("""
+         Langchain is a powerful framework designed to streamline the development of applications using Language Models (LLMs). It provides a comprehensive integration of various components, simplifying the process of assembling them to create robust applications.
+         Leveraging the power of Langchain, the creation of chatbots becomes effortless.
+         """)
