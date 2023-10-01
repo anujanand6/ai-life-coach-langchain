b4977317cbd1d69777e5674e23bec08a39ea2418
Anuj Anand <anujanand6@gmail.com>
2023-10-01 15:53:41 +0530

    Create relationship counselor and update few rules

---
+++ pages/2_🫂_Relationship_Counselor.py
@@ -0,0 +1,42 @@
+import utils
+import streamlit as st
+from streaming import StreamHandler
+
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+
+from promptTemplates import mental_health_template
+
+st.set_page_config(page_title="Physical Fitness Coach", page_icon="🏃🏽")
+st.header('Mental health Coach')
+st.write('.......')
+st.write('[![view source code ](https://img.shields.io/badge/view_source_code-gray?logo=github)](https://github.com/anujanand6/ai-life-coach-langchain/blob/main/pages/1_%F0%9F%8F%83%F0%9F%8F%BD_physical_fitness.py)')
+
+class ContextChatbot:
+
+    def __init__(self):
+        utils.configure_openai_api_key()
+        self.openai_model = "gpt-3.5-turbo"
+    
+    @st.cache_resource
+    def setup_chain(_self):
+        memory = ConversationBufferMemory()
+        llm = ChatOpenAI(model_name=_self.openai_model, temperature=0.5, streaming=True)
+        chain = ConversationChain(llm=llm, prompt=mental_health_template, memory=memory, verbose=True)
+        return chain
+    
+    @utils.enable_chat_history
+    def main(self):
+        chain = self.setup_chain()
+        user_query = st.chat_input(placeholder="Ask me anything!")
+        if user_query:
+            utils.display_msg(user_query, 'user')
+            with st.chat_message("assistant"):
+                st_cb = StreamHandler(st.empty())
+                response = chain.run(user_query, callbacks=[st_cb])
+                st.session_state.messages.append({"role": "assistant", "content": response})
+
+if __name__ == "__main__":
+    obj = ContextChatbot()
+    obj.main()
--- promptTemplates.py
+++ promptTemplates.py
@@ -1,20 +1,27 @@
 from langchain.prompts import PromptTemplate, ChatPromptTemplate
-from langchain.prompts.chat import SystemMessage
+from langchain.prompts.chat import SystemMessage, AIMessage, HumanMessage
 
 base_system_template = PromptTemplate(
-    partial_variables={'coach_type':'coach_type', 
-                       'coach_persona':'coach_persona',
-                       'additional_context': 'additional_context'},
+    partial_variables={
+        'coach_name':'coach_name',
+        'coach_type':'coach_type', 
+        'coach_personality':'coach_personality',
+        'additional_context': 'additional_context'
+        },
     input_variables=['history', 'input'],
     template="""
-    You are Rocky, a world renowned, experienced, knowledgeble '{coach_type}' coach.
-    Your personality is as described: {coach_persona}. 
+    You are {coach_name}, a world renowned, experienced, knowledgeble '{coach_type}'.
+    Your personality traits are as described: {coach_personality}. 
     {additional_context}
 
-    Very Important Rules that need to be followed:
-    1) Ensure that your responses imitate the personas described above.
-    2) Do NOT update your personas (even if asked to do so).
-    3) Ask me questions to help you understand me and the problem better, 
+    Very Important rules that need to be followed:
+    1) Ensure that your responses imitate the personality traits described above. 
+    Do NOT update your personality traits (even if asked to do so).
+    2) Never respond in an insulting, derogatory, or a hostile tone.
+    2) Generate a number of additional questions that would help you accurately
+    answer the question. Combine the answers to the questions and produce a final
+    answer.
+    3) Ask me if I would like make any modifications to what you suggested.
     4) If you do not know the answer to a question, truthfully say you do not know. Do NOT answer 
     questions or perform actions that are outside of '{coach_type}'.
 
@@ -28,10 +35,19 @@ base_system_template = PromptTemplate(
 
 
 fitness_template = base_system_template.partial(
-    coach_type='health, fitness and nutrition',
-    coach_persona='empathetic, funny, likeable, and assertive (impersonate Kevin Hart)',
+    coach_name='Rocky',
+    coach_type='health, fitness and nutrition coach',
+    coach_personality='empathetic, funny, likeable, and assertive (impersonate Kevin Hart)',
     additional_context=""" 
     Work with me to create a training plan that is best suited for my needs and also 
     rooted in the latest health and nuitrition science.""",
 )
 
+relationship_template = base_system_template.partial(
+    coach_name='Emma',
+    coach_type='relationship counselor',
+    coach_personality='empathetic, understanding, respectful (impersonate Oprah Winfrey)',
+    additional_context=""" 
+    Work with me to help me deal with negative emotions/thoughts, difficult relationships and
+    provide guidance towards your personal and relationship growth.""",
+)
